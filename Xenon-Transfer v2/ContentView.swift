4f175d91d4b81184cddb9e95c6549dbf403eaad6
Myles Conti <myles@contibros.com>
2025-03-24 16:22:51 +1100

    Initial Commit

---
+++ Xenon-Transfer v2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xenon-Transfer v2
+//
+//  Created by STUDIO ONE on 24/3/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xenon-Transfer v2Tests/Xenon_Transfer_v2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Xenon_Transfer_v2Tests.swift
+//  Xenon-Transfer v2Tests
+//
+//  Created by STUDIO ONE on 24/3/2025.
+//
+
+import Testing
+@testable import Xenon_Transfer_v2
+
+struct Xenon_Transfer_v2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
